f99fb73f5c64ef5fe661f0f48480a66b218df903
Zhian N. Kamvar <zkamvar@gmail.com>
2023-03-24 11:28:32 -0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#! /bin/env bash
+
+wd=$(pwd)
+cd renv-16 && \
+  Rscript -e 'renv::restore()' && \
+  Rscript script.R 2>&1 | tee out.txt && 
+  cd ${wd} \
+  || echo "something went wrong"
+
+cd renv-17 && \
+  Rscript -e 'renv::restore()' && \
+  Rscript script.R 2>&1 | tee out.txt && 
+  cd ${wd} \
+  || echo "something went wrong"
+
+
+
